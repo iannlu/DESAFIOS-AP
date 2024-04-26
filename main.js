eb6fcfacc8e08eb7f1e1de40a7b8466c501e8492
LucaRomero <luca.romero.et7@gmail.com>
2024-04-26 11:43:18 -0300

    Desafio 2

---
+++ main.js
@@ -0,0 +1,11 @@
+let nombreProducto = "Alfajor";
+let PrecioUnitario = "500";
+
+let CantidadDeseada = prompt('¿Cuantos alfajores desea llevar?');
+
+
+
+
+let resto = CantidadDeseada * PrecioUnitario;
+
+alert('Son en total ' + "$"+resto + " por los " + CantidadDeseada + " alfajores");
